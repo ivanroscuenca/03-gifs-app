9924e2e0bf3a57c8bb2754075de0c39d25204982
ivanroscuenca <ivanros@protonmail.com>
2023-08-04 21:16:23 +0200

    video90http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  constructor(private http: HttpClient) {}
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
   private _tagsHistory: string[] = [];
   private apiKey: string = 'c9eEQXIdX7OlsdZ5ERbcUExdlAdEfklD';
 
@@ -26,7 +29,14 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
-  }
 
-  constructor() {}
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    this.http.get(`${this.serviceUrl}/search`, { params }).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
