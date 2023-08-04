d0fe7a8310e32bdcc720be9ef22c352369b84c6e
ivanroscuenca <ivanros@protonmail.com>
2023-08-04 18:54:12 +0200

    video87GifsService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { GifsModule } from './gifs/services/gifs.module';
+import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,15 +1,20 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
 })
 export class SearchBoxComponent {
+  //inyectamos servicio en el contructor
+  constructor(private gifsService: GifsService) {}
+
   @ViewChild('txtTagInput')
   tagInput!: ElementRef<HTMLInputElement>;
 
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({ newTag });
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
   }
 }
